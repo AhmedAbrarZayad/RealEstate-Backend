bbd62def2c14b061e53ba01421590abde6f30882
Ahmed Abrar <abrarjubah82@gmail.com>
2025-11-12 22:25:08 +0600

    Corrected some logic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const admin = require('firebase-admin');
 require('dotenv').config({ path: '../.env' });
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("../realestate-fbcf5-firebase-adminsdk-fbsvc-0aded39002.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -21,7 +21,29 @@ app.use(cors());
 app.use(express.json());
 
 // Middleware
+async function verifyFirebaseJWTToken(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
 
+    const idToken = authHeader.split(' ')[1];
+
+    if(!idToken){
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken; 
+    next();
+  } catch (error) {
+    console.error('Error verifying Firebase ID token:', error);
+    res.status(403).json({ message: 'Unauthorized or invalid token' });
+  }
+}
+
+// APIS
 app.get('/', (req, res) => {
     res.send(`${mongodbPass}`);
 });
@@ -61,7 +83,7 @@ async function run() {
         res.send(result);
     });
 
-    app.get('/users/:id/properties', async (req, res) => {
+    app.get('/users/:id/properties', verifyFirebaseJWTToken, async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const user = await usersCollection.findOne(query);
@@ -71,7 +93,7 @@ async function run() {
         res.send(properties);
     });
 
-    app.patch('/users/:id/properties/:propertyId', async (req, res) => {
+    app.patch('/users/:id/properties/:propertyId', verifyFirebaseJWTToken, async (req, res) => {
         const id = req.params.id;
         const propertyId = req.params.propertyId;
         const newProperty = req.body;
@@ -89,13 +111,13 @@ async function run() {
         const result = await propertyCollection.updateOne({ _id: new ObjectId(propertyId) }, updateProperty);
     })
 
-    app.delete('/users/:id/properties/:propertyId', async (req, res) => {
+    app.delete('/users/:id/properties/:propertyId', verifyFirebaseJWTToken, async (req, res) => {
         const query = { _id: new ObjectId(req.params.propertyId) };
         const result = await propertyCollection.deleteOne(query);
         res.send(result);
     });
     // Property APIs
-    app.post('/property', async (req, res) => {
+    app.post('/property', verifyFirebaseJWTToken, async (req, res) => {
         const email = req.query.email;
         console.log(req.body);
         const property = req.body;
@@ -108,10 +130,13 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/property', async (req, res) => {
+    app.get('/property', verifyFirebaseJWTToken, async (req, res) => {
         const email = req.query.email;
         const query = {};
         if(email){
+            if(email !== req.user.email){
+                return res.status(403).json({ message: 'Forbidden access' });
+            }
             query.email = email;
         }
         const cursor = propertyCollection.find(query);
@@ -127,16 +152,20 @@ async function run() {
     })
 
     // Review APIs
-    app.post('/reviews', async (req, res) => {
+    app.post('/reviews', verifyFirebaseJWTToken, async (req, res) => {
         const review = req.body;
         const result = await reviewsCollection.insertOne(review);
         res.send(result);
     });
-    app.get('/reviews', async (req, res) => {
+    app.get('/reviews', verifyFirebaseJWTToken, async (req, res) => {
         const email = req.query.email;
         const query = {}
         if(email){
-            query.email = email;
+            if(email !== req.user.email){
+                return res.status(403).json({ message: 'Forbidden access' });
+            }
+            const user = await usersCollection.findOne({ email: email });
+            query.reviewerId = user._id.toString();
         }
         const cursor = reviewsCollection.find(query);
         const reviews = await cursor.toArray();
@@ -149,8 +178,16 @@ async function run() {
         const reviews = await cursor.toArray();
         res.send(reviews);
     });
-    app.patch('/reviews/:id', async (req, res) => {
+    app.patch('/reviews/:id', verifyFirebaseJWTToken, async (req, res) => {
+        const userEmail = req.user.email;
+        const cursor = usersCollection.find({ email: userEmail });
+        const user = await cursor.toArray();
+        const userId = user[0]._id;
         const id = req.params.id;
+        const review = await reviewsCollection.findOne({ _id: new ObjectId(id) });
+        if(review.reviewerId.toString() !== userId.toString()){
+            return res.status(403).json({ message: 'Forbidden access' });
+        }
         const updatedReview = req.body;
         const updateDoc = {
             $set: {
